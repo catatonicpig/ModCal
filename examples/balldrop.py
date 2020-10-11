af200e087d902c88d13348e278b58a723e691326
M Plumlee <mplumlee.data@gmail.com>
2020-10-11 13:47:37 -0500

    Adding in core framework drafts
    This worked on the first try and I am amazed.

--- examples/balldrop.py
+++ examples/balldrop.py
@@ -9,52 +9,18 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from statfuncs.sampling import postsampler
+from statfuncs.emulation import emulator
 from testfuncs.balldrop import balldropmodel_linear,\
     balldropmodel_quad, balldropmodel_drag, balldroptrue
 
-# t1 = np.array(range(1,12,1))/10
-# t1 = np.reshape(t1, (t1.shape[0], -1))
-# h1 = 10*np.ones(t1.shape)
-# t2 =  np.array(range(1,30,4))/10
-# t2 = np.reshape(t2, (t2.shape[0], -1))
-# h2 = 40*np.ones(t2.shape)
-# t3 =  np.array(range(1,20,3))/10
-# t3 = np.reshape(t3, (t3.shape[0], -1))
-# h3 = 20*np.ones(t3.shape)
-# x1 = np.append(t1, h1, 1)
-# x2 = np.append(t2, h2, 1)
-# x3 = np.append(t3, h3, 1)
-# x = np.vstack((x1,x2,x3))
-# y = balldroptrue(x) + np.random.normal(0, np.sqrt(sigma2), x.shape[0])
+def lpriorphys(theta):
+    return (sps.gamma.logpdf(theta[:,0], 20, 0, 1/2) +
+            sps.gamma.logpdf(theta[:,1], 2, 0, 20))
+def dpriorphys(n):
+    return np.vstack((sps.gamma.rvs(20, 0, 1/2, size=n),
+                     sps.gamma.rvs(2, 0, 20, size=n))).T
 
-
-y = np.array([[11.5],
-       [11.2],
-       [ 9.6],
-       [ 7.9],
-       [ 9.6],
-       [ 6.8],
-       [ 7.3],
-       [ 7.9],
-       [ 6.5],
-       [ 2.2],
-       [ 2.8],
-       [21.6],
-       [20.3],
-       [18.8],
-       [12.1],
-       [11.8],
-       [10.6],
-       [ 3.9],
-       [40.4],
-       [36.2],
-       [36. ],
-       [32.6],
-       [24.8],
-       [19.7],
-       [13.1],
-       [ 5.8]])
-x = np.array([[ 0.1, 10. ],
+xdata = np.array([[ 0.1, 10. ],
        [ 0.2, 10. ],
        [ 0.3, 10. ],
        [ 0.4, 10. ],
@@ -81,126 +47,137 @@ x = np.array([[ 0.1, 10. ],
        [ 2.5, 40. ],
        [ 2.9, 40. ]])
 
-
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots() 
-ax.plot(x[:,0],y,'o')
-
-y = np.squeeze(y)
-sigma2 = 4
-theta0 = np.reshape(np.array((10,10)), (1, 2))
-theta_lin = np.reshape(np.array((1, 2.5, 5, 10, 15, 20)), (6, -1))
-f_lin = balldropmodel_linear(theta_lin, x)
-
-theta_quad = np.reshape(np.array((5, 10, 15)), (3, -1))
-f_quad = balldropmodel_quad(theta_quad, x)
-
-theta_drag = np.array(((1, 5), (10, 5),
-                       (15, 1), (10, 20), (5, 20)))
-f_drag = balldropmodel_drag(theta_drag, x)
-
-
 #NEED TO EMULATE EACH MODEL
 
+thetacompexp = dpriorphys(100)
+fcompexp = balldropmodel_quad(thetacompexp, xdata) 
 
-#NEED TO CALIBRATE EACH MODEL
+emu = emulator(thetacompexp, fcompexp)
+thetacomptest = dpriorphys(100)
+ftest = balldropmodel_quad(thetacomptest, xdata) 
+Y = emu.predict(thetacomptest)[0]
+print(np.sqrt(np.mean((Y-ftest)**2)))
+# #NEED TO CALIBRATE EACH MODEL
+# import matplotlib.pyplot as plt
+# sigma2 = 4
+# y = np.array([[11.5],
+#        [11.2],
+#        [ 9.6],
+#        [ 7.9],
+#        [ 9.6],
+#        [ 6.8],
+#        [ 7.3],
+#        [ 7.9],
+#        [ 6.5],
+#        [ 2.2],
+#        [ 2.8],
+#        [21.6],
+#        [20.3],
+#        [18.8],
+#        [12.1],
+#        [11.8],
+#        [10.6],
+#        [ 3.9],
+#        [40.4],
+#        [36.2],
+#        [36. ],
+#        [32.6],
+#        [24.8],
+#        [19.7],
+#        [13.1],
+#        [ 5.8]])
+# y = np.squeeze(y)
+# fig, ax = plt.subplots() 
+# ax.plot(x[:,0],y,'o')
 
-print(y - balldropmodel_quad(np.array((10,40))[None,:], x))
-print(y-balldroptrue(x))
-#Prior on thetaasa
-def lpriorphys(theta):
-    return (sps.gamma.logpdf(theta[:,0], 20, 0, 1/2) +
-            sps.gamma.logpdf(theta[:,1], 2, 0, 20))
-def dpriorphys(n):
-    return np.vstack((sps.gamma.rvs(20, 0, 1/2, size=n),
-                     sps.gamma.rvs(2, 0, 20, size=n))).T
-thetaprior = dpriorphys(1000)
+# print(y - balldropmodel_quad(np.array((10,40))[None,:], x))
+# print(y-balldroptrue(x))
+# #Prior on thetaasa
 
-def llik1(theta):
-    return -0.5*np.sum((balldropmodel_quad(theta, x) - y) ** 2 / 1 ** 2,1)
-thetapost1 = postsampler(dpriorphys(1000), lpriorphys, llik1)
+# def llik1(theta):
+#     return -0.5*np.sum((balldropmodel_quad(theta, x) - y) ** 2 / 1 ** 2,1)
+# thetapost1 = postsampler(dpriorphys(1000), lpriorphys, llik1)
 
-def llikclosed(theta):
-    resid1 = balldropmodel_quad(theta, x) - y
-    resid2 = balldropmodel_linear(theta, x) - y
-    resid3 = balldropmodel_drag(theta, x) - y
-    term1 = -0.5*np.sum((resid1.T) ** 2 / np.sqrt(sigma2),0)
-    term2 = -0.5*np.sum((resid2.T) ** 2 / np.sqrt(sigma2),0)
-    term3 = -0.5*np.sum((resid3.T) ** 2 / np.sqrt(sigma2),0)
-    terms = np.vstack((term1,term2,term3))
-    tm = np.max(terms,0)
-    termsadj = terms - tm
-    logpost = tm + np.log(np.sum(np.exp(termsadj),0))
-    return logpost
-thetapostclosed = postsampler(dpriorphys(1000), lpriorphys, llikclosed)
+# def llikclosed(theta):
+#     resid1 = balldropmodel_quad(theta, x) - y
+#     resid2 = balldropmodel_linear(theta, x) - y
+#     resid3 = balldropmodel_drag(theta, x) - y
+#     term1 = -0.5*np.sum((resid1.T) ** 2 / np.sqrt(sigma2),0)
+#     term2 = -0.5*np.sum((resid2.T) ** 2 / np.sqrt(sigma2),0)
+#     term3 = -0.5*np.sum((resid3.T) ** 2 / np.sqrt(sigma2),0)
+#     terms = np.vstack((term1,term2,term3))
+#     tm = np.max(terms,0)
+#     termsadj = terms - tm
+#     logpost = tm + np.log(np.sum(np.exp(termsadj),0))
+#     return logpost
+# thetapostclosed = postsampler(dpriorphys(1000), lpriorphys, llikclosed)
 
 
-def lpriorstat(phi):
-    return (sps.gamma.logpdf(phi[:,0], 4, 0, 1) +
-            sps.gamma.logpdf(phi[:,1], 4, 0, 1) +
-            sps.gamma.logpdf(phi[:,2], 4, 0, 1))
-def dpriorstat(n):
-    return np.vstack((sps.gamma.rvs(4, 0, 1, size=n),
-                     sps.gamma.rvs(4, 0, 1, size=n),
-                     sps.gamma.rvs(4, 0, 1, size=n))).T
+# def lpriorstat(phi):
+#     return (sps.gamma.logpdf(phi[:,0], 4, 0, 1) +
+#             sps.gamma.logpdf(phi[:,1], 4, 0, 1) +
+#             sps.gamma.logpdf(phi[:,2], 4, 0, 1))
+# def dpriorstat(n):
+#     return np.vstack((sps.gamma.rvs(4, 0, 1, size=n),
+#                      sps.gamma.rvs(4, 0, 1, size=n),
+#                      sps.gamma.rvs(4, 0, 1, size=n))).T
 
-def lprioropen(thetaphi):
-    return (lpriorphys(thetaphi[:,:2]) + lpriorstat(thetaphi[:,2:]))
-def dprioropen(n):
-    return np.vstack((dpriorphys(n).T, dpriorstat(n).T)).T
+# def lprioropen(thetaphi):
+#     return (lpriorphys(thetaphi[:,:2]) + lpriorstat(thetaphi[:,2:]))
+# def dprioropen(n):
+#     return np.vstack((dpriorphys(n).T, dpriorstat(n).T)).T
 
 
-#speed up computation
-CorrMatDeltaT = np.exp(-2*np.abs(np.subtract.outer(x[:,0],x[:,0])))
-CorrMatDeltaH = np.exp(-1/40*np.abs(np.subtract.outer(x[:,1],x[:,1])))
-CorrMatDelta = CorrMatDeltaT * CorrMatDeltaH
-W, V = np.linalg.eigh(CorrMatDelta)
+# #speed up computation
+# CorrMatDeltaT = np.exp(-2*np.abs(np.subtract.outer(x[:,0],x[:,0])))
+# CorrMatDeltaH = np.exp(-1/40*np.abs(np.subtract.outer(x[:,1],x[:,1])))
+# CorrMatDelta = CorrMatDeltaT * CorrMatDeltaH
+# W, V = np.linalg.eigh(CorrMatDelta)
 
-def loglikopen(thetaphi):
-    theta = thetaphi[:,:2]
-    phi = thetaphi[:,2:]
-    Sigobs = np.eye(x.shape[0])
+# def loglikopen(thetaphi):
+#     theta = thetaphi[:,:2]
+#     phi = thetaphi[:,2:]
+#     Sigobs = np.eye(x.shape[0])
     
-    resid1 = balldropmodel_quad(theta, x) - y
-    resid2 = balldropmodel_linear(theta, x) - y
-    resid3 = balldropmodel_drag(theta, x) - y
+#     resid1 = balldropmodel_quad(theta, x) - y
+#     resid2 = balldropmodel_linear(theta, x) - y
+#     resid3 = balldropmodel_drag(theta, x) - y
     
-    #speed up computation
-    resid1half = resid1 @ V
-    resid2half = resid2 @ V
-    resid3half = resid3 @ V
-    term1 = -0.5*np.sum((resid1.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,0])))) ** 2,0)
-    term2 = -0.5*np.sum((resid2.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,1])))) ** 2,0)
-    term3 = -0.5*np.sum((resid3.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,2])))) ** 2,0)
-    term1 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,0]))),0)
-    term2 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,1]))),0)
-    term3 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,2]))),0)
-    terms = np.vstack((term1, term2, term3))
-    tm = np.max(terms,0)
-    termsadj = terms - tm
-    logpost = tm + np.log(np.sum(np.exp(termsadj),0))
-    return logpost
+#     #speed up computation
+#     resid1half = resid1 @ V
+#     resid2half = resid2 @ V
+#     resid3half = resid3 @ V
+#     term1 = -0.5*np.sum((resid1.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,0])))) ** 2,0)
+#     term2 = -0.5*np.sum((resid2.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,1])))) ** 2,0)
+#     term3 = -0.5*np.sum((resid3.T / np.sqrt(np.abs(sigma2+np.multiply.outer(W, phi[:,2])))) ** 2,0)
+#     term1 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,0]))),0)
+#     term2 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,1]))),0)
+#     term3 -= 0.5*np.sum(np.log(np.abs(sigma2+np.multiply.outer(W, phi[:,2]))),0)
+#     terms = np.vstack((term1, term2, term3))
+#     tm = np.max(terms,0)
+#     termsadj = terms - tm
+#     logpost = tm + np.log(np.sum(np.exp(termsadj),0))
+#     return logpost
 
-thetapostopen = postsampler(dprioropen(1000), lprioropen, loglikopen)
+# thetapostopen = postsampler(dprioropen(1000), lprioropen, loglikopen)
 
-print(np.mean(thetapostopen,0))
+# print(np.mean(thetapostopen,0))
 
 
-fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(21, 5))
+# fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(21, 5))
 
-from scipy.stats import kde
-def two2d(axis, theta):
-    nbins = 50
-    k = kde.gaussian_kde(theta.T)
-    xi, yi = np.mgrid[6:13:nbins*1j, 0:60:nbins*1j]
-    zi = k(np.vstack([xi.flatten(), yi.flatten()]))
-    axis.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='gouraud', cmap=plt.cm.BuGn_r)
-    axis.contour(xi, yi, zi.reshape(xi.shape))
+# from scipy.stats import kde
+# def two2d(axis, theta):
+#     nbins = 50
+#     k = kde.gaussian_kde(theta.T)
+#     xi, yi = np.mgrid[6:13:nbins*1j, 0:60:nbins*1j]
+#     zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+#     axis.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='gouraud', cmap=plt.cm.BuGn_r)
+#     axis.contour(xi, yi, zi.reshape(xi.shape))
 
-two2d(axes[0], thetaprior)
-two2d(axes[1], thetapostclosed)
-two2d(axes[2], thetapostopen[:,:2])
-#NEED TO DO SOME MODEL MIXING
+# two2d(axes[0], thetaprior)
+# two2d(axes[1], thetapostclosed)
+# two2d(axes[2], thetapostopen[:,:2])
+# #NEED TO DO SOME MODEL MIXING
 
-#NEED TO DECIDE A NEW HEIGHT TO DROP IT AT
+# #NEED TO DECIDE A NEW HEIGHT TO DROP IT AT
--- statfuncs/emulation.py
+++ statfuncs/emulation.py
@@ -1,37 +1,81 @@
 # -*- coding: utf-8 -*-
 """Header here."""
 import numpy as np
+import importlib
+import copy
 
-
-class emulation(object):
+class emulator(object):
     """Emulator."""
 
-    def __init__(self):
-        self._means = None
-        self._stds = None
-
-    def build(self, codepointer):
-        if self._means is None:  # during training only
-            self._means = np.mean(data, axis=0)
-
-        if self._stds is None:  # during training only
-            self._stds = np.std(data, axis=0)
-            if not self._stds.all():
-                raise ValueError('At least one column has standard deviation of 0.')
+    def __init__(self, theta=None, f=None, x=None, software='plumleeemu',
+                 buildfirst=True, testpred=True):
+        if f is None or theta is None:
+            raise ValueError('You have no provided any theta and/or f.')
         
-        return (data - self._means) / self._stds
-
-    def preprocess(self, data):
-        if self._means is None:  # during training only
-            self._means = np.mean(data, axis=0)
-
-        if self._stds is None:  # during training only
-            self._stds = np.std(data, axis=0)
-            if not self._stds.all():
-                raise ValueError('At least one column has standard deviation of 0.')
+        if f.ndim < 0.5 or f.ndim > 2.5:
+            raise ValueError('f must have either 1 or 2 demensions.')
+            
+        if theta.ndim < 0.5 or theta.ndim > 2.5:
+            raise ValueError('theta must have either 1 or 2 demensions.')
         
-        return (data - self._means) / self._stds
-
+        if theta.shape[0] < 2 * theta.shape[1]:
+            raise ValueError('theta should have at least 2 more' +
+                             'rows than columns.')
+            
+        if f.shape[0] is not theta.shape[0]:
+            raise ValueError('The rows in f must match' +
+                             ' the rows in theta')
+            
+        if f.ndim == 1 and x is not None:
+            raise ValueError('Cannot use x if f has a single column.')
+        
+        if f.ndim == 2 and x is not None and\
+            f.shape[1] is not x.shape[0]:
+            raise ValueError('The columns in f must match' +
+                             ' the rows in x')
+        
+        self.theta = theta
+        self.f = f
+        self.x = x
+        
+        try:
+            emusoftware = importlib.import_module('statfuncs.emulationsubfuncs.' + software)
+        except:
+            raise ValueError('Module not found!')
+            
+            
+        if "build" not in dir(emusoftware):
+            raise ValueError('Function \"build\" not found in module!')
+        
+        self.builder = emusoftware.build
+            
+        if buildfirst:
+            self.build()
+            self.modelbuilt = True
+            
+        if "predict" not in dir(emusoftware):
+            raise ValueError('Function \"predict\" not found in module!')
+            
+        self.predictor = emusoftware.predict
+        
+        if testpred:
+            if self.modelbuilt is False:
+                print('Cannot test prediction because \"buildfirst\" is False.')
+            else:
+                self.predictor(self.model,
+                               copy.deepcopy(self.theta),
+                               options=None)
+        
+    def build(self, options=None):
+        self.model = self.builder(copy.deepcopy(self.theta),
+                                  copy.deepcopy(self.f), 
+                                  copy.deepcopy(self.x),
+                                  copy.deepcopy(options))
+        
+    def predict(self, theta, options=None):
+        return self.predictor(self.model,
+                              copy.deepcopy(theta),
+                              copy.deepcopy(options))
 
 def loglik(emumodel, theta, y=None, S=None):
     """
