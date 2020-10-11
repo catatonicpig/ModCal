0486e7c7a2c0a949b857ae203bcbce50aaefbd38
Matt P <mplumlee.data@gmail.com>
2020-10-11 11:59:07 -0500

    Some updates
    Nothing but getting this repo going.

--- statfuncs/emulation.py
+++ statfuncs/emulation.py
@@ -1,30 +1,64 @@
 # -*- coding: utf-8 -*-
 """Header here."""
+import numpy as np
 
 
+class emulation(object):
+    """Emulator."""
+
+    def __init__(self):
+        self._means = None
+        self._stds = None
+
+    def build(self, codepointer):
+        if self._means is None:  # during training only
+            self._means = np.mean(data, axis=0)
+
+        if self._stds is None:  # during training only
+            self._stds = np.std(data, axis=0)
+            if not self._stds.all():
+                raise ValueError('At least one column has standard deviation of 0.')
+        
+        return (data - self._means) / self._stds
+
+    def preprocess(self, data):
+        if self._means is None:  # during training only
+            self._means = np.mean(data, axis=0)
+
+        if self._stds is None:  # during training only
+            self._stds = np.std(data, axis=0)
+            if not self._stds.all():
+                raise ValueError('At least one column has standard deviation of 0.')
+        
+        return (data - self._means) / self._stds
 
 
-def semu_logpost(semumodel, theta, options=None):
+def loglik(emumodel, theta, y=None, S=None):
     """
     Return posterior of function evaluation at the new parameters.
 
     Parameters
     ----------
-    semumodel : dict
-        Simple emulator model of (f(x_i,\theta) - y_i) / \sigma_i
-    thetaorig : array of float
+    emumodel : Pred
+        A fitted emulator model defined as an emulation class.
+    theta : array of float
         Some matrix of parameters where function evaluations as starting points.
+    y : Observations
+        A vector of the same length as x with observations. 'None' is equivlent to a vector of
+        zeros.
+    S : Observation Covariance Matrix
+        A matrix of the same length as x with observations. 'None' is equivlent to the
+        identity matrix.
+
     Returns
     -------
     post: vector of unnormlaized log posterior
     """
     if theta.ndim == 1:
         theta = theta.reshape((1,theta.shape[0]))
+    
     loglik = emulation_smart_loglik(semumodel, theta)
-    #logprior = -4 * np.sum((theta-0.5) ** 2,1)
     
-    logpost = loglik# + logprior
-    
-    #sumofExp = post - np.log(np.sum(np.exp(post - np.max(post))))
+    logpost = loglik
     
     return logpost
--- statfuncs/sampling.py
+++ statfuncs/sampling.py
@@ -3,44 +3,6 @@
 import numpy as np
 from statfuncs.samplingsubfuncs.plumleesamp import plumleepostsampler
 
-
-
-
-def semu_pred(semumodel, theta, options=None):
-    """
-    Return predicted values of function evaluation at the new parameters.
-
-    Parameters
-    ----------
-    semumodel : dict
-        Simple emulator model, as described above.
-    thetanew : array of float
-        New parameter where function evaluations should be sampled.
-    options : dict, optional
-        Prediction options, as a dictionary with entries
-
-        * `'returnvariance'`: boolean if predictive variance is returned.
-        The default is False.
-
-        * `'returnvariance'`: boolean if predictive covariance matrix is 
-        returned. The default is False.
-        
-    Returns
-    -------
-    fhat : array of float
-        Predicted values of recovered function evaluation, of size
-        (`Nnew`, `n`) where `Nnew` is the number of new
-        parameters, `n` is the number of input.
-    predvar_inclremoval : array of float
-        Predictive variances/covariances for the function evaluations.
-    """
-    if theta.ndim == 1:
-        theta = theta.reshape((1,theta.shape[0]))
-    
-    predmean, predvar = emulation_smart_prediction(semumodel, theta, options)
-    return predmean, predvar
-
-
 def postsampler(thetastart, logpriorfunc, loglikfunc, options=None):
     """
     Return draws from the posterior.
