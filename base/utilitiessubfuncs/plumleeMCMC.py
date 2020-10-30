b49b5c0bf90b6f44f56bb5d0923f52d010606c35
Matt P <mplumlee.data@gmail.com>
2020-10-30 11:35:44 -0500

    Even more small changes to plumleeMCMC

--- base/utilitiessubfuncs/plumleeMCMC.py
+++ base/utilitiessubfuncs/plumleeMCMC.py
@@ -43,7 +43,7 @@ def plumleepostsampler(thetastart, logpostfunc, numsamp, tarESS):
             keepgoing = False
     rho = 0.5
     jitter = 0.01
-    numsamppc = np.ceil(np.minimum(200,np.maximum(25,numsamp/numchain)).astype('int')
+    numsamppc = np.minimum(200,np.maximum(25,np.ceil(numsamp/numchain))).astype('int')
     for iters in range(0,maxiters):
         covmat0 = np.cov(thetasave.T)
         Wc,Vc = np.linalg.eigh(covmat0)
